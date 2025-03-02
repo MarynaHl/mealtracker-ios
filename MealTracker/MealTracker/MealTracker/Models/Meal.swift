da1c4b728f7f2563c088dbb8f4dee7fc18147d96
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-03-02 20:56:46 +0000

    feat: Create Meal model with single date property

--- MealTracker/MealTracker/MealTracker/Models/Meal.swift
+++ MealTracker/MealTracker/MealTracker/Models/Meal.swift
@@ -3,25 +3,22 @@ import SwiftUI
 
 struct Meal: Identifiable {
     let id: UUID
-    var name: String       // Назва (за замовчуванням "Прийом їжі", або "Сніданок", тощо)
-    var startDate: Date    // Дата та час початку
-    var endDate: Date      // Дата та час завершення
-    var color: Color       // Колір з SwiftUI
-    var note: String       // Нотатка (додатковий коментар)
-    
+    var date: Date          // Одна дата/час прийому
+    var name: String        // Назва/тип ("Сніданок", "Обід" тощо)
+    var note: String        // Нотатка
+    var color: Color        // Колір іконки чи позначення (якщо треба)
+
     init(
         id: UUID = UUID(),
+        date: Date,
         name: String = "Прийом їжі",
-        startDate: Date,
-        endDate: Date,
-        color: Color = Color.blue,
-        note: String = ""
+        note: String = "",
+        color: Color = Color.pink
     ) {
         self.id = id
+        self.date = date
         self.name = name
-        self.startDate = startDate
-        self.endDate = endDate
-        self.color = color
         self.note = note
+        self.color = color
     }
 }
