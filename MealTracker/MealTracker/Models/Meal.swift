0fdac73f461651761acadfd09b378f6ced9d2215
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-03-02 20:17:15 +0000

    feat: Create Meal model

--- MealTracker/MealTracker/Models/Meal.swift
+++ MealTracker/MealTracker/Models/Meal.swift
@@ -1,8 +1,27 @@
-//
-//  Meal.swift
-//  MealTracker
-//
-//  Created by Maryna on 02/03/2025.
-//
-
 import Foundation
+import SwiftUI
+
+struct Meal: Identifiable {
+    let id: UUID
+    var name: String       // Назва (за замовчуванням "Прийом їжі", або "Сніданок", тощо)
+    var startDate: Date    // Дата та час початку
+    var endDate: Date      // Дата та час завершення
+    var color: Color       // Колір з SwiftUI
+    var note: String       // Нотатка (додатковий коментар)
+    
+    init(
+        id: UUID = UUID(),
+        name: String = "Прийом їжі",
+        startDate: Date,
+        endDate: Date,
+        color: Color = Color.blue,
+        note: String = ""
+    ) {
+        self.id = id
+        self.name = name
+        self.startDate = startDate
+        self.endDate = endDate
+        self.color = color
+        self.note = note
+    }
+}
