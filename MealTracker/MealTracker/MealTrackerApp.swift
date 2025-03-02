98c74414c9113df10e566e07f281d004f5a7931a
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-03-02 20:27:02 +0000

    fix: Set MainView as the entry point of the app

--- MealTracker/MealTracker/MealTrackerApp.swift
+++ MealTracker/MealTracker/MealTrackerApp.swift
@@ -1,17 +1,10 @@
-//
-//  MealTrackerApp.swift
-//  MealTracker
-//
-//  Created by Maryna on 02/03/2025.
-//
-
 import SwiftUI
 
 @main
 struct MealTrackerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
